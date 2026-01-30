aa8a2273766b9a52c9277f008bae47f816df398e
Chukwuma Ukaha <chukwumaukaha@gmail.com>
2026-01-30 13:51:22 +0100

    implement GET all subjects route

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import express, { Request, Response} from 'express';
+import subjectsRouter from "./routes/subjects";
+import cors from "cors";
 
 const app = express();
 const PORT = 8000;
 
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}))
+
 // Middleware to parse incoming JSON requests
 app.use(express.json());
+
+app.use('/api/subjects', subjectsRouter);
 
 // Root GET route
 app.get('/', (req: Request, res: Response) => {
