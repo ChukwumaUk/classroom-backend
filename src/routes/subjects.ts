3788f7c899b9b5b0da1df001f2286cf25a3bab44
Chukwuma Ukaha <chukwumaukaha@gmail.com>
2026-02-05 11:26:01 +0100

    updating extensions in the routes file

--- src/routes/subjects.ts
+++ src/routes/subjects.ts
@@ -1,7 +1,7 @@
 import { and, desc, eq, getTableColumns, ilike, or, sql } from "drizzle-orm";
 import express,  { Request, Response } from "express";
-import { departments, subjects } from "../db/schema";
-import { db } from "../db";
+import { departments, subjects } from "../db/schema/app.js";
+import { db } from "../db/index.js";
 
 const router = express.Router();
 
